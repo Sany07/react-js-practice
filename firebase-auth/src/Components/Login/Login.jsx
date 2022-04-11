e7d0c2bef8d0a56d9f805d1bac38edfe01eeda9a
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-12 03:18:37 +0600

    reg updated

--- firebase-auth/src/Components/Login/Login.jsx
+++ firebase-auth/src/Components/Login/Login.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleFormSubmit = () => alert("s");
   return (
     <Container>
       <Row>
         <Col md={6} className="mx-auto">
-          <Form>
+          <Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                onChange={handleEmail}
+                type="email"
+                placeholder="Enter email"
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -17,13 +28,17 @@ const Login = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                onChange={handlePassword}
+                type="password"
+                placeholder="Password"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Login
             </Button>
           </Form>
         </Col>
--- firebase-auth/src/Components/Register/Register.jsx
+++ firebase-auth/src/Components/Register/Register.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import app from "../../firebase.init";
+import { getAuth } from "firebase/auth";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+const auth = getAuth(app);
 
 const Register = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+  };
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {console.log(user.user.email)}</p>
+      </div>
+    );
+  }
   return (
     <Container>
       <Row>
         <Col md={6} className="mx-auto">
-          <Form>
+          <Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Enter email"
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -17,13 +52,17 @@ const Register = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Login
             </Button>
           </Form>
         </Col>
