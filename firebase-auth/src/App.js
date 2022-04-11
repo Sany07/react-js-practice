2e97b54765ea15cd871d8615118500686ceeb01f
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-12 02:13:52 +0600

    updated

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,19 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home.jsx";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 
 function App() {
   return (
-    <div className="">
+    <>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- firebase-auth/src/Components/Login/Login.jsx
+++ firebase-auth/src/Components/Login/Login.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-    </Form>
+    <Container>
+      <Row>
+        <Col md={6} className="mx-auto">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
