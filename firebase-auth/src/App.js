3abeafe3e0bcf0c7c9583270429f5d4d6d125ec6
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-12 01:54:01 +0600

    updated

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home.jsx";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
